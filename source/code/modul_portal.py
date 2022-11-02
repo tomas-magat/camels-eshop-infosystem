20dfc750088bcddcf88426f4d3231c59a640f434
tomas-magat <tom.magatpk@gmail.com>
2022-11-02 17:44:15 +0100

    Updated Modul portal

--- source/code/modul_portal.py
+++ source/code/modul_portal.py
@@ -4,16 +4,15 @@ from utils import *
 goods = read_file('tovar')
 version = get_version('tovar')
 
-
-# Update 'goods' variable every 3 seconds
 def update_goods():
     global goods, version
     
     current_version = get_version('tovar')
 
     if current_version != version:
-        print('y')
         goods = read_file('tovar')
         version = current_version
 
+
+# Update 'goods' variable every 3 seconds
 run_periodically(update_goods, 3)
