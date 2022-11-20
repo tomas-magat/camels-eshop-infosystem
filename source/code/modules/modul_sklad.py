a72ea3b25025005da5ff71ea056e05ff27f56f6d
lukassrnka <luki.srnka@gmail.com>
2022-11-20 22:44:22 +0100

    sklad

--- source/code/modules/modul_sklad.py
+++ source/code/modules/modul_sklad.py
@@ -1,1 +1,5 @@
-'a'
+# Modul Sklad -
+# Show amount of products.
+# Alerts you to products with low quantity.
+# Can generate (automatic/half-automatic/manual) order,
+# creating objednavka_[id_transakcie].txt
