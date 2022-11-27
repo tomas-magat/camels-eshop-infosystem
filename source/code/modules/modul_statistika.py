dbecbefaa21c55ef4f0382920866e71a8eeb2c27
Picklemaster09 <branis161@gmail.com>
2022-11-27 01:30:43 +0100

    statistika

--- source/code/modules/modul_statistika.py
+++ source/code/modules/modul_statistika.py
@@ -2,6 +2,7 @@ from utils.file import DataFile
 from utils.ui_commands import UI_Commands
 from utils import tools
 import matplotlib.pyplot as plt
+import matplotlib.style
 
 
 
@@ -16,7 +17,7 @@ class Statistika:
         # Track button clicks
         self.commands.button_click(
             self.ui.statistikaButton, self.switch_screen)
-        self.statistika_test()
+        self.statistika()
         # Read file 'tovar.txt'
         # self.tovar = DataFile('tovar')
         # self.goods = self.tovar.read()
@@ -27,22 +28,21 @@ class Statistika:
 
     def switch_screen(self):
         """Redirect to this statistika screen."""
-
         self.commands.redirect(self.ui.statistika)
 
-    def statistika_test(self):
-        # data
+    def statistika(self):
         x = [i for i in range(10)]
         y = [i/2 for i in range(10)]
         y1 = [i**2 for i in range(10)]
         c = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-        m1 = sorted([15, 9, 420, 69, 25, 90, 63, 78, 54, 75], reverse=True)
-        m2 = sorted([420, 5, 69, 20, 25, 90, 63, 78, 54, 75])
-        # plt.style.use(['seaborn-v0_8-notebook'])
+        m1 = sorted([15, 9, 80, 69, 25, 90, 63, 78, 54, 75], reverse=True)
+        m2 = sorted([70, 5, 69, 20, 25, 90, 63, 78, 54, 75])
         font = {'fontname':'Arial'}
         edgecolor = '#757575'
         linewidth = 2
         graph_color = '#FFFFFF'
+        text = '150ks\nza 5dni\nnohavice'
+        
 
         najviac, a1 = plt.subplots(figsize=[4.9,3.15],linewidth=linewidth, edgecolor=edgecolor)
         najviac.patch.set_facecolor(graph_color)
@@ -53,10 +53,37 @@ class Statistika:
         a1.axes.xaxis.set_ticklabels([])
         a1.tick_params(axis='x', which='both', length=0)
         a1.set_title('Najpredavanejsie produkty',**font,fontsize=15,weight='bold')
-        a1.bar(c, m1)
+        bars1 = a1.bar(c, m1)
+        
+        annot1 = a1.annotate("",xy=(0,0),xytext=(0,10),textcoords='offset points',ha='center',
+                    bbox=dict(boxstyle="round", fc='white', alpha=0.9, ec="#E47E0A", lw=2))
+        annot1.set_visible(False)
+        
+        def update_annot1(event):
+            b=abs((plt.xlim()[0]*-1)+plt.xlim()[1])
+            d=abs((plt.ylim()[0]*-1)+plt.ylim()[1])
+            x = b/0.775*event.x/260-2.5
+            y = d/0.77*event.y/175.275-10
+            annot1.xy = (x,y)
+            annot1.set_text(text)
+        def hover1(event):
+            vis = annot1.get_visible()
+            if event.inaxes == a1:
+                for bar in bars1:
+                    cont, i = bar.contains(event)
+                    if cont:
+                        update_annot1(event)
+                        annot1.set_visible(True)
+                        najviac.canvas.draw_idle()
+                        return
+                    else:
+                        if vis:
+                            annot1.set_visible(False)
+                            najviac.canvas.draw_idle()
+        najviac.canvas.mpl_connect("motion_notify_event", hover1)
         # a1.spines['left'].set_visible(False)
         # a1.spines['bottom'].set_visible(False)
-
+    
         najmenej, a2 = plt.subplots(figsize=[4.9,3.15],linewidth=linewidth, edgecolor=edgecolor)
         najmenej.patch.set_facecolor(graph_color)
         a2.set_facecolor(graph_color)
@@ -65,7 +92,35 @@ class Statistika:
         a2.axes.xaxis.set_ticklabels([])
         a2.tick_params(axis='x', which='both', length=0)
         a2.set_title('Najmenej predavane produkty',**font,fontsize=15,weight='bold')
-        a2.bar(c, m2)
+        bars2 = a2.bar(c, m2)
+
+        annot2 = a2.annotate("",xy=(0,0),xytext=(0,10),textcoords='offset points',ha='center',
+                    bbox=dict(boxstyle="round", fc='white', alpha=0.9, ec="#E47E0A", lw=2))
+        annot2.set_visible(False)
+    
+        def update_annot2(event):
+            b=abs((plt.xlim()[0]*-1)+plt.xlim()[1])
+            d=abs((plt.ylim()[0]*-1)+plt.ylim()[1])
+            x = b/0.775*event.x/260-2.5
+            y = d/0.77*event.y/175.275-10
+            annot2.xy = (x,y)
+            annot2.set_text(text)
+        def hover2(event):
+            vis = annot2.get_visible()
+            if event.inaxes == a2:
+                for bar in bars2:
+                    cont, i = bar.contains(event)
+                    if cont:
+                        update_annot2(event)
+                        annot2.set_visible(True)
+                        najmenej.canvas.draw_idle()
+                        return
+                    else:
+                        if vis:
+                            annot2.set_visible(False)
+                            najmenej.canvas.draw_idle()
+        najmenej.canvas.mpl_connect("motion_notify_event", hover2)
+
 
         vyvoj_ceny, a3 = plt.subplots(figsize=[7.18,3.21],linewidth=linewidth, edgecolor=edgecolor)
         vyvoj_ceny.set_facecolor(graph_color)
@@ -79,26 +134,27 @@ class Statistika:
         a3.plot(x, y, label='naklady')
         a3.plot(x, y1,label='vynosy')
         a3.legend(frameon=False,fontsize=15)
-
-
+    
         
+    
         plt.tight_layout()
-
-        c = '-0,05%'
-        b = '#FF0000'
-        f = '23,58€'
         self.commands.plot_graph(self.ui.najviacGraf, najviac)
         self.commands.plot_graph(self.ui.najmenejGraf, najmenej)
         self.commands.plot_graph_trzby(self.ui.trzbyNaklady, vyvoj_ceny)
 
+        c = '-0,05%'
+        percenta = '#FF0000'
+        cisla = '#2C57D8'
+        f = '23,58€'
+
         self.ui.label_6.setText(c)
-        self.ui.label_6.setStyleSheet('color:'+b)
+        self.ui.label_6.setStyleSheet('color:'+percenta)
         self.ui.label_20.setText(f)
         self.ui.label_10.setText('2 678')
-        self.ui.label_10.setStyleSheet('color:#2596be')
+        self.ui.label_10.setStyleSheet('color:'+cisla)
         self.ui.label_12.setText('Sobotu (87)')
-        self.ui.label_12.setStyleSheet('color:#2596be')
+        self.ui.label_12.setStyleSheet('color:'+cisla)
         self.ui.label_16.setText('Topanok (865 parov)')
-        self.ui.label_16.setStyleSheet('color:#2596be')
+        self.ui.label_16.setStyleSheet('color:'+cisla)
         self.ui.label_14.setText('2003-09-10 10-34-59;kosela;5ks;5.99/ks')
-        self.ui.label_14.setStyleSheet('color:#2596be')
+        self.ui.label_14.setStyleSheet('color:'+cisla)
